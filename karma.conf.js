4d5e9ef2ade77f3bb985fd554f1a3983078dd8b3
Brad Chen <brad@bradchen.com>
2017-08-17 00:00:03 -0400

    Make Karma run slightly faster.

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,9 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    webpack: {
+      devtool: 'eval'
+    }
   });
 };
